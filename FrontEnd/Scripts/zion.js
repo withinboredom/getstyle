749f12cfcebe4366f4a6e15bc009aba87afb3947
Rob Landers <landers.robert@gmail.com>
2014-01-23 02:47:23 -0500

    upated features and added smooth scrolling

--- FrontEnd/Scripts/zion.js
+++ FrontEnd/Scripts/zion.js
@@ -1,20 +1,35 @@
-$(document).ready(function() {
-  $('#navbar').affix();
-  $('.dropdown-toggle').dropdown();
-  $('[data-toggle=tooltip]').tooltip();
-  function parallax(){
-    var scrolled = $(window).scrollTop();
-    if(scrolled >= 0) {
-      $('.container-wrapper-home .summary').css('top', (scrolled * 0.5) + 'px');
-      var opacity = scrolled / 100;
-      var opacity = opacity / 2;
-      $('.container-wrapper-home .summary').css('opacity', 1 - opacity);
+$(document).ready(function () {
+    $('#navbar').affix();
+    $('.dropdown-toggle').dropdown();
+    $('[data-toggle=tooltip]').tooltip();
+    function parallax() {
+        var scrolled = $(window).scrollTop();
+        if (scrolled >= 0) {
+            $('.container-wrapper-home .summary').css('top', (scrolled * 0.5) + 'px');
+            var opacity = scrolled / 100;
+            var opacity = opacity / 2;
+            $('.container-wrapper-home .summary').css('opacity', 1 - opacity);
+        }
     }
-  }
-  var x = $(window).width();
-  if(x > 767) {
-    $(window).scroll(function(e){
-      parallax();
+    var x = $(window).width();
+    if (x > 767) {
+        $(window).scroll(function (e) {
+            parallax();
+        });
+    }
+});
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
     });
-  }
 });
